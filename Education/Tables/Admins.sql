0ed4835becef78e19e713023c8382e0b476e0115
abbas <avavav060@gmail.com>
2025-05-27 19:27:16 +0430

    feat : create Admins table

---
+++ Education/Tables/Admins.sql
@@ -0,0 +1,13 @@
+USE UniversityPortalDB
+go
+DROP TABLE IF EXISTS Admins;
+
+CREATE TABLE Admins (
+    AdminID INT PRIMARY KEY,
+	UserID INT,			--fk
+    Firstname NVARCHAR(50) NOT NULL,
+    Lastname NVARCHAR(50) NOT NULL,
+    Role_user NVARCHAR(30) NOT NULL,         
+    PhoneNumber VARCHAR(15),                        
+    Email NVARCHAR(100),   
+);
