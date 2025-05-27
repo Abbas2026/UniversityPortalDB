f08f38fa2e81f2b1fcd23e729d88ca406e2178ff
abbas <avavav060@gmail.com>
2025-05-27 19:17:03 +0430

    feat : create Userlink table

---
+++ Education/Tables/Userlink.sql
@@ -0,0 +1,9 @@
+USE UniversityPortalDB
+go
+DROP TABLE IF EXISTS Userlink;
+
+CREATE TABLE Userlink (
+    UserlinkID INT PRIMARY KEY,
+    EntityType NVARCHAR(50) NOT NULL,
+    EntityID INT NOT NULL,			--fk
+);
