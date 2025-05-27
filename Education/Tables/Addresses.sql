25246663a2eca74439c3a470c285def452b74bc9
abbas <avavav060@gmail.com>
2025-05-27 17:19:25 +0430

    feat : create Addresses table

---
+++ Education/Tables/Addresses.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Addresses (
+    AddressID INT PRIMARY KEY,
+	Country NVARCHAR(50) NOT NULL,       
+    Province NVARCHAR(50) NOT NULL,       
+    City NVARCHAR(50) NOT NULL,            
+    Street NVARCHAR(100),                
+    Alley NVARCHAR(100),     
+	PlaqueNumber NVARCHAR(10),             
+    PostalCode CHAR(10),                   
+    ExtraDescription NVARCHAR(255)        
+);
