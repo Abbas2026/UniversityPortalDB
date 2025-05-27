f2a1256fc64330751e4da4337a790cf1979199b2
abbas <avavav060@gmail.com>
2025-05-27 19:12:12 +0430

    feat : create UserAccounts table

---
+++ Education/Tables/UserAccounts.sql
@@ -0,0 +1,12 @@
+USE UniversityPortalDB
+go
+DROP TABLE IF EXISTS UserAccounts;
+
+CREATE TABLE UserAccounts (
+    UserID INT PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    UserPassword NVARCHAR(255) NOT NULL, 
+    Role_user NVARCHAR(30) NOT NULL,         
+    IsActive BIT NOT NULL DEFAULT 1,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+);
