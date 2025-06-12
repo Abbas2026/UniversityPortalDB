e49e446766d32ffc655cc1062016f431518d1810
pouya-prh <pouya.bishe2@gmail.com>
2025-06-13 02:21:44 +0430

    insert member Sample Data

---
+++ Library/DataSamples/insert/Members_insertData.sql
@@ -0,0 +1,23 @@
+USE UniversityPortalDB
+
+INSERT INTO Library.Members (StudentID, FullName) VALUES
+(1001, 'Ali Moradi'),
+(1002, 'Sara Ahmadi'),
+(1003, 'Reza Karimi'),
+(1004, 'Narges Rahimi'),
+(1005, 'Hossein Abbasi'),
+(1006, 'Zahra Khosravi'),
+(1007, 'Amir Ranjbar'),
+(1008, 'Mina Eskandari'),
+(1009, 'Sina Tavakoli'),
+(1010, 'Arezoo Shahbazi'),
+(1011, 'Hamed Ebrahimi'),
+(1012, 'Ladan Kazemi'),
+(1013, 'Farhad Sharifi'),
+(1014, 'Samira Alizadeh'),
+(1015, 'Mohammad Najafi'),
+(1016, 'Parisa Javadi'),
+(1017, 'Mehdi Ghaffari'),
+(1018, 'Elham Shariat'),
+(1019, 'Kaveh Yousefi'),
+(1020, 'Shima Asgari');
