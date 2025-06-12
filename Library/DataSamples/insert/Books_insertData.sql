db55882dfee7fad819420ecc255cc7a859a7e359
pouya-prh <pouya.bishe2@gmail.com>
2025-06-13 00:45:35 +0430

    inserted sample data into tables

---
+++ Library/DataSamples/insert/Books_insertData.sql
@@ -0,0 +1,31 @@
+USE UniversityPortalDB
+
+INSERT INTO Library.Books (Title, ISBN, PublisherID, CategoryID, PublishYear) VALUES
+('Digital Logic Design', '9780131989269', 1, 1, 2020),
+('Computer Networks', '9780132126953', 2, 2, 2018),
+('Data Structures and Algorithms', '9780262033848', 3, 1, 2017),
+('Operating System Concepts', '9781118063330', 4, 3, 2019),
+('Introduction to Electric Circuits', '9780073529553', 5, 4, 2016),
+('Microprocessor Architecture', '9780130153667', 6, 4, 2021),
+('Database System Concepts', '9780073523323', 7, 3, 2015),
+('Artificial Intelligence: A Modern Approach', '9780136042594', 8, 5, 2022),
+('Programming Language Pragmatics', '9780124104099', 9, 3, 2019),
+('Embedded Systems Design', '9781118824713', 10, 4, 2020),
+('Computer Organization and Design', '9780124077263', 11, 4, 2018),
+('Algorithms Unlocked', '9780262518802', 12, 1, 2017),
+('Digital Signal Processing', '9780131988422', 13, 4, 2016),
+('Software Engineering', '9780135974445', 14, 3, 2021),
+('Computer Graphics: Principles and Practice', '9780321399526', 15, 5, 2018),
+('Introduction to Automata Theory', '9780321455369', 16, 1, 2019),
+('Wireless Communications', '9780470546320', 17, 4, 2020),
+('Network Security Essentials', '9780136097041', 18, 2, 2022),
+('Machine Learning', '9780071808552', 19, 5, 2021),
+('Digital Communication', '9780073529554', 20, 4, 2015),
+('Operating Systems: Internals and Design Principles', '9780134670959', 21, 3, 2020),
+('Principles of Compiler Design', '9788120313553', 22, 3, 2017),
+('Introduction to Computer Vision', '9783319326873', 23, 5, 2019),
+('Advanced Computer Architecture', '9780128119051', 24, 4, 2022),
+('Cloud Computing Concepts', '9780124046276', 25, 2, 2021);
+
+
+
---
+++ Library/DataSamples/insert/Categories_insertData.sql
@@ -0,0 +1,13 @@
+USE UniversityPortalDB
+
+INSERT INTO Library.Categories (Name) VALUES
+('Computer Fundamentals'),
+('Computer Networks'),
+('Software Engineering'),
+('Hardware and Electronic Circuits'),
+('Artificial Intelligence and Machine Learning'),
+('Databases'),
+('Operating Systems'),
+('Computer Graphics'),
+('Information Security'),
+('Computer Architecture');
