c71f79e4d5aa44ebef4d8f26065387835aac0750
pouya-prh <pouya.bishe2@gmail.com>
2025-06-13 20:04:05 +0430

    trg_AfterInsert_Borrowing created and tested

---
+++ Library/Triggers/trg_AfterInsert_Borrowing.sql
@@ -0,0 +1,12 @@
+USE UniversityPortalDB
+GO
+CREATE TRIGGER trg_AfterInsert_Borrowing
+ON Library.Borrowing
+AFTER INSERT
+AS
+BEGIN
+    UPDATE bc
+    SET bc.IsAvailable = 0
+    FROM Library.BookCopies bc
+    JOIN inserted i ON bc.CopyID = i.CopyID
+END;
---
+++ Library/Triggers/trg_AfterInsert_Borrowing_Test.sql
@@ -0,0 +1,10 @@
+USE UniversityPortalDB
+
+select *
+from Library.BookCopies
+
+--select *
+--from Library.Members
+
+--INSERT INTO Library.Borrowing (MemberID, CopyID, BorrowDate, DueDate)
+--VALUES (1, 1, GETDATE(), DATEADD(DAY, 14, GETDATE()));
